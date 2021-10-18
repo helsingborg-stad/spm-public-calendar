9b5aca80e4de64bd3c34837c988ee788571f1b09
Tomas Green <tomas.green@gmail.com>
2021-10-18 15:59:16 +0200

    Added packages tests

--- Tests/PublicCalendarTests/PublicCalendarTests.swift
+++ Tests/PublicCalendarTests/PublicCalendarTests.swift
@@ -1,11 +1,65 @@
-    import XCTest
-    @testable import PublicCalendar
+import XCTest
+import Combine
+@testable import PublicCalendar
 
-    final class PublicCalendarTests: XCTestCase {
-        func testExample() {
-            // This is an example of a functional test case.
-            // Use XCTAssert and related functions to verify your tests produce the correct
-            // results.
-            XCTAssertEqual(PublicCalendar().text, "Hello, World!")
-        }
+var cancellables = Set<AnyCancellable>()
+final class PublicCalendarTests: XCTestCase {
+    func testIsHoliday() {
+        let expectation = XCTestExpectation(description: "testIsHoliday")
+        let c = PublicCalendar(years: [2019], fetchAutomatically: true, previewData: false)
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        let date = df.date(from: "2019-12-25")!
+        c.fetch().sink { compl in
+            switch compl {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+                expectation.fulfill()
+            case .finished:debugPrint("finished")
+            }
+        } receiveValue: { db in
+            XCTAssert(db.isHoliday(date: date))
+            expectation.fulfill()
+        }.store(in: &cancellables)
+        wait(for: [expectation], timeout: 20.0)
     }
+    func testIsNotHoliday() {
+        let expectation = XCTestExpectation(description: "testIsNotHoliday")
+        let c = PublicCalendar(years: [2019], fetchAutomatically: true, previewData: false)
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd"
+        let date = df.date(from: "2019-12-23")!
+        c.fetch().sink { compl in
+            switch compl {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+                expectation.fulfill()
+            case .finished:debugPrint("finished")
+            }
+        } receiveValue: { db in
+            XCTAssert(db.isHoliday(date: date) == false)
+            expectation.fulfill()
+        }.store(in: &cancellables)
+        wait(for: [expectation], timeout: 20.0)
+    }
+    func testNotFound() {
+        let expectation = XCTestExpectation(description: "testNotFound")
+        let c = PublicCalendar(years: [2019], fetchAutomatically: true, previewData: false)
+        let df = DateFormatter()
+        c.purge()
+        df.dateFormat = "yyyy-MM-dd"
+        let date = df.date(from: "2021-12-24")!
+        c.fetch().sink { compl in
+            switch compl {
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+                expectation.fulfill()
+            case .finished:debugPrint("finished")
+            }
+        } receiveValue: { db in
+            XCTAssert(db.events(on: date).isEmpty)
+            expectation.fulfill()
+        }.store(in: &cancellables)
+        wait(for: [expectation], timeout: 20.0)
+    }
+}
